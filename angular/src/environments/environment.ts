c1a36a8699d13d9cbc7b8283a98c12226a532bad
Jatin_Gandhi <jatingandhi7461@gmail.com>
2024-03-25 23:44:03 +0530

    added environment variables

---
+++ angular/src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+    orgEvents: "orgEvents",
+    userId: "userId",
+    companyName: "companyName",
+    token: "token",
+    addUserURL: "http://localhost:4000/user-api/users",
+    addOrgURL: "http://localhost:4000/org-api/organizations",
+    getOrgsURL: "http://localhost:4000/org-api/organizations/",
+    addEventURL: "http://localhost:4000/event-api/events",
+    getEventsURL: "http://localhost:4000/event-api/events",
+    getUsersURL: "http://localhost:4000/user-api/users?email=",
+    userLoginURL: "http://localhost:4000/user-api/login",
+    loginOrgURL: "http://localhost:4000/org-api/login",
+    getUserWithIdURL: "http://localhost:4000/user-api/users/",
+    updateUserWithIdURL: "http://localhost:4000/user-api/users/",
+    deleteEventWithIdURL: "http://localhost:4000/event-api/events/",
+    updateEventURL: "http://localhost:4000/event-api/events/",
+    verifyTokenURL: "http://localhost:4000/event-api/verify"
+};
